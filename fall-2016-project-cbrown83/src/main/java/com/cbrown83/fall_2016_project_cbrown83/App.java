991ad9f9d1477222286e0abf219404ba16b8ad6f
Courtney Brown <cbrown@pcp134640pcs.wireless.calpoly.edu>
2016-11-02 15:53:43 -0700

    adding maven project

---
+++ fall-2016-project-cbrown83/src/main/java/com/cbrown83/fall_2016_project_cbrown83/App.java
@@ -0,0 +1,13 @@
+package com.cbrown83.fall_2016_project_cbrown83;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
