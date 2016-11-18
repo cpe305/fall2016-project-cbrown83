abecdd120fb680ce369370fe8bf8384571a9e949
Courtney Brown <cbrown@Courtneys-MacBook-Pro.local>
2016-11-17 17:09:46 -0800

    updated pom file for JSON dependency and added JSON reader skeleton

---
+++ src/main/java/JSONReader.java
@@ -0,0 +1,10 @@
+import java.io.FileReader;
+import java.util.Iterator;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JSONReader {
+
+}
